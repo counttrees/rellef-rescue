6d27c584ce4cb342d103dcaa5fe1f52d9dfd6386
jhasketan <jasjena@gmail.com>
2018-08-17 22:53:09 +0530

    form field

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  charityCounttrees:
+        {
+          apiKey: "AIzaSyDRi2lHuZHlNB6fude47LbFDEcr-9V04BA",
+          authDomain: "relief-rescue-8b888.firebaseapp.com",
+          databaseURL: "https://relief-rescue-8b888.firebaseio.com",
+          projectId: "relief-rescue-8b888",
+          storageBucket: "relief-rescue-8b888.appspot.com",
+          messagingSenderId: "12686065933"
+        }
 };
